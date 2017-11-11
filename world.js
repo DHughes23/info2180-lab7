a920625ae7f6b37af4778785669551f2f930d61c
Daniel Hughes <danielhughes23@hotmail.com>
2017-11-11 01:22:15 +0000

    Completed lab exercise

--- world.js
+++ world.js
@@ -7,8 +7,32 @@ function starterCode(){
 }
 
 function callName(){
+    var item = new XMLHttpRequest();
     var caller = document.getElementById('country').value;
-    var phpUrl = 'https://info2180-lab7-dhughes23.c9users.io/world.php?country=' + caller;
+    var checker = document.getElementById("forAll").checked;
+    
+    if (checker===true){
+        var phpUrl = 'https://info2180-lab7-dhughes23.c9users.io/world.php?all=true';
+    }else if(caller){
+        var phpUrl = 'https://info2180-lab7-dhughes23.c9users.io/world.php?country=' + caller;
+    }else{
+        document.getElementById("result").innerHTML='';
+    }
+    item.onreadystatechange = function(){
+                if (item.readyState === XMLHttpRequest.DONE) {
+                    if (item.status === 200){
+                        var respond = item.responseText;
+                        document.getElementById("result").innerHTML=respond;
+                    }
+                }
+            }
+            item.open('GET', phpUrl);
+            item.send();
+}
+
+function returnAll(){
+    var caller = document.getElementById('country').value;
+    var phpUrl = 'https://info2180-lab7-dhughes23.c9users.io/world.php?all=true';
     var item = new XMLHttpRequest();
     
     item.onreadystatechange = function(){
--- world.php
+++ world.php
@@ -5,10 +5,17 @@ $host = getenv('IP');
 $username = getenv('C9_USER');
 $password = '';
 $dbname = 'world';
+$everything = $_GET['all'];
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
 
-$stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+if ($country){
+    $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+}elseif($everything){
+    $stmt = $conn->query("SELECT * FROM countries");
+}else {
+    echo '';
+}
 
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
@@ -18,4 +25,17 @@ foreach ($results as $row) {
 }
 echo '</ul>';
 
+
+/*
+$everything = $GET['all'];
+
+$stm = $cnn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+
+$result = $stm->fetchAll(PDO::FETCH_ASSOC);
+
+echo '<ul>';
+foreach ($result as $row) {
+  echo '<li>' . $row['name'] . ' is ruled by ' . $row['head_of_state'] . '</li>';
+}
+echo '</ul>';*/
 
